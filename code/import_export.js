ccb5cf062c94a17042aee9805711172af409948a
Debottam Kar <debottamkar2003@gmail.com>
2025-11-20 17:44:43 +0530

    import export

---
+++ code/import_export.js
@@ -0,0 +1,10 @@
+export let name = "debottam";
+export function greet() {
+    return `Hello, ${name}!`;
+}
+// export expects statement or declaration
+// export default expects expression
+
+const age = 22;
+
+export default age;
