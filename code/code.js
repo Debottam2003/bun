c8525a3e6a3bf53fe7a7f11318b66c28457fd3ed
Debottam Kar <debottamkar2003@gmail.com>
2025-11-24 18:17:58 +0530

    testing added

--- code/code.js
+++ code/code.js
@@ -1,10 +1,35 @@
-let { log: print } = console;
+// let { log: print } = console;
 
-let arr = ["ronaldo", "messi", "neymar", "mbappe"];
-console.log(arr);
+// let arr = ["ronaldo", "messi", "neymar", "mbappe"];
+// console.log(arr);
 
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// print("hello world!");
+
+const number = (num) => {
+    if (num > 0) {
+        return `positive`;
+    }
+    else if (num < 0) {
+        return `nagative`;
+    }
+    else {
+        return `zero`;
+    }
 }
 
-print("hello world!");
+// Testing the function
+function testNumber(val) {
+    if (number(val[0]) === val[1]) {
+        console.log(`Test passed for input ${val[0]}`);
+    } else {
+        console.log(`Test failed for input ${val[0]}`);
+    }
+}
+
+testNumber([5, `positive`]);
+testNumber([-3, `nagative`]);
+testNumber([0, `zero`]);
