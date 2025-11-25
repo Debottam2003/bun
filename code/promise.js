7711ac3f8e4b71ea2778ca7707232eb7c0c481e3
Debottam Kar <debottamkar2003@gmail.com>
2025-11-25 21:16:02 +0530

    promise

---
+++ code/promise.js
@@ -0,0 +1,18 @@
+let p = Promise.resolve(42);
+console.log(p);
+
+p.then(value => {
+    console.log("Resolved with value:", value);
+});
+
+async function asyncFunction() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Async function resolved!");
+        }, 1000);
+    });
+}
+
+let res = await asyncFunction();// res is a Promise object of pending state
+// let res = await asyncFunction();// res is the message that passed in the resolve 
+console.log(res);
